57e0c436c3ec8d0e7877c359b227922528945ffc
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-09-22 17:15:39 -0600

    feat: Mongodb configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Cargar variables del .env
+    MongooseModule.forRoot(process.env.MONGODB_URI), // Leer el URI desde .env
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
