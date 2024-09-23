70e43a6685e4bb955e028864039cfd320c4efed7
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-09-23 17:57:31 -0600

    feat: controller,service and schema

--- src/audits/controller/audits.controller.ts
+++ src/audits/controller/audits.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuditsService } from '../service/audits.service';
 
 @Controller('audits')
-export class AuditsController {}
+export class AuditsController {
+  constructor(private readonly auditsService: AuditsService) {}
+
+  @Post()
+  async auditUrl(@Body('url') url: string) {
+    const auditResult = await this.auditsService.auditUrl(url);
+    return auditResult;
+  }
+}
--- src/audits/service/audits.service.ts
+++ src/audits/service/audits.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuditsService {}
+export class AuditsService {
+  async auditUrl(url: string): Promise<any> {
+    // Importación dinámica de chrome-launcher
+    const chromeLauncher = (await import('chrome-launcher')).default;
+
+    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+
+    const options = {
+      logLevel: 'info' as const,
+      output: 'json' as const,
+      onlyCategories: ['performance', 'accessibility', 'seo'],
+      port: chrome.port,
+    };
+
+    // Importación dinámica de lighthouse
+    const lighthouse = (await import('lighthouse')).default;
+
+    const runnerResult = await lighthouse(url, options);
+
+    const report = runnerResult.lhr;
+
+    await chrome.kill();
+
+    return {
+      performance: report.categories.performance.score * 100,
+      accessibility: report.categories.accessibility.score * 100,
+      seo: report.categories.seo.score * 100,
+      reportDetails: report.audits,
+    };
+  }
+}
